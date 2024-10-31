01fa7d90308f8c7a7a7bb251fa75bafdffdf5fa6
Hans-Christian Otto <c.otto@suora.com>
2024-10-31 13:41:01 +0100

    Extrract components

--- src/demo/step3-magic-table/MagicTableExample.tsx
+++ src/demo/step3-magic-table/MagicTableExample.tsx
@@ -1,70 +1,8 @@
-import React, { Children, isValidElement, PropsWithChildren } from "react";
-
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Field } from "./Field";
 import { LogMessage } from "../LogMessage";
 
-import {
-  ResourceListContextProvider,
-  useResourceListContext,
-} from "./ResourceListContext";
-import { ResourceContextProvider } from "./ResourceContext";
-
-const MagicTable: React.FunctionComponent<PropsWithChildren> = ({
-  children,
-}) => {
-  const { resources } = useResourceListContext();
-  const columns = Children.toArray(children).filter(
-    (
-      child,
-    ): child is React.ReactElement<
-      React.ComponentProps<typeof MagicTableColumn>
-    > => isValidElement(child) && child.type === MagicTableColumn,
-  );
-
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          {columns.map((column) => (
-            <TableHead>{column.props.name}</TableHead>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {resources.map((resource) => (
-          <ResourceContextProvider resource={resource}>
-            <TableRow>
-              {columns.map((column) => (
-                <MagicTableColumn {...column.props} />
-              ))}
-            </TableRow>
-          </ResourceContextProvider>
-        ))}
-      </TableBody>
-    </Table>
-  );
-};
-
-type MagicTableColumnProps = {
-  field: string;
-  name: string;
-};
-
-const MagicTableColumn = ({ field }: MagicTableColumnProps) => {
-  return (
-    <TableCell>
-      <Field name={field} />
-    </TableCell>
-  );
-};
+import { ResourceListContextProvider } from "./ResourceListContext";
+import { MagicTable } from "./MagicTable";
+import { MagicTableColumn } from "./MagicTableColumn";
 
 export const MagicTableExample: React.FunctionComponent = () => {
   return (
